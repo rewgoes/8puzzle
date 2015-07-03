d27ef4b1434a4fb64e02bec99898bc198e4f62a5
Rafael Wolf de Goes <rewgoes@gmail.com>
2015-07-02 21:37:08 -0300

    Update test.js

--- nbproject/test.js
+++ nbproject/test.js
@@ -1,3 +1,7 @@
-function myFunction() {
-    document.getElementById("addVar").style.backgroundColor = "#614B4A";
+function loadDaFun() {
+   var script = document.createElement('script');
+   script.src = 'http://form.jotformz.com/jsform/51826828820663';
+   script.type = 'text/javascript';
+   var head = document.getElementsByTagName("head")[0];
+   head.appendChild(script);
 };
